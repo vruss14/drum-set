892b4cc3c33f66dd46cf8c53b1f370a8714e250b
Valerie Russell <vruss14@gmail.com>
2022-01-05 13:25:30 -0500

    click events trigger audio; all tests passing

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// Trigger audio & color change only for correct key press events
+
 $("body").keypress(function(event){
-    let keyPressed = event.key;
+    let keyPressed = event.key.toLowerCase();
 
     $(".drum-pad").each(function() {
-        if(keyPressed === this.id) {
+        if(keyPressed === this.innerText.toLowerCase()) {
             $(this).css('background-color', 'green')
             setTimeout(revertColor, 250, this)
-            $('#display').text(this.id.toUpperCase())
+            $('#display').text(this.innerText)
+            $(`#${this.innerText}`)[0].play();
         }
     });
-
 });
+
+// Revert color after 0.25 seconds
 
 function revertColor(element) {
     $(element).css('background-color', 'transparent');
 }
+
+// Click events trigger the same kind of response as keypress events
+
+$('.drum-pad').click(function(event) {
+    console.log(event.target.innerText);
+    $(`#${event.target.id}`).css('background-color', 'green')
+    setTimeout(revertColor, 250, `#${event.target.id}`)
+    $('#display').text(event.target.innerText)
+    $(`#${event.target.innerText}`)[0].play();
+  });
